a3b3415539a1356096bd544fa64e60abb63ea2e4
Tehedor <sergiot1805@gmail.com>
2023-12-26 20:15:40 +0100

    control_chart

--- 4.5_helm_chart/contro_chart.py
+++ 4.5_helm_chart/contro_chart.py
@@ -4,7 +4,7 @@ from subprocess import call
 call(["helm","repo","add","stehedor","https://tehedor.github.io/helmcharts/"])
 
 
-call(["helm", "install" , "helm", "stehedor/details"])
-call(["helm", "install" , "helm", "stehedor/productpage"])
-call(["helm", "install" , "helm", "stehedor/ratings"])
-call(["helm", "install" , "helm", "stehedor/reviews"])
+call(["helm", "install" , "helm_details", "stehedor/details"])
+call(["helm", "install" , "helm_productpage", "stehedor/productpage"])
+call(["helm", "install" , "helm_ratings", "stehedor/ratings"])
+call(["helm", "install" , "helm_reviews", "stehedor/reviews"])
