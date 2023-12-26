08a72e2a06ba8ae82eea9706feb13c06516f4ed2
Tehedor <sergiot1805@gmail.com>
2023-12-26 20:19:07 +0100

    Nuevos comando para contro_cahr

--- 4.5_helm_chart/contro_chart.py
+++ 4.5_helm_chart/contro_chart.py
@@ -1,10 +1,28 @@
 from subprocess import call
-
+from os import sys
 # helm repo add stehedor https://tehedor.github.io/helmcharts/
-call(["helm","repo","add","stehedor","https://tehedor.github.io/helmcharts/"])
+
+segundo_argumento = ""
+
+if not len(sys.argv) < 2:
+    segundo_argumento = sys.argv[1]
+
+if segundo_argumento == "add_repo":
+    call(["helm","repo","add","stehedor","https://tehedor.github.io/helmcharts/"])
+    exit(0)
+
+if segundo_argumento == "eliminar_todo":
+    call(["helm", "uninstall", "helmDetails"])
+    call(["helm", "uninstall", "helmProductpage"])
+    call(["helm", "uninstall", "helmRatings"])
+    call(["helm", "uninstall", "helmReviews"])
 
 
-call(["helm", "install" , "helm_details", "stehedor/details"])
-call(["helm", "install" , "helm_productpage", "stehedor/productpage"])
-call(["helm", "install" , "helm_ratings", "stehedor/ratings"])
-call(["helm", "install" , "helm_reviews", "stehedor/reviews"])
+    exit(0)
+
+
+
+call(["helm", "install" , "helmDetails", "stehedor/details"])
+call(["helm", "install" , "helmProductpage", "stehedor/productpage"])
+call(["helm", "install" , "helmRatings", "stehedor/ratings"])
+call(["helm", "install" , "helmReviews", "stehedor/reviews"])
