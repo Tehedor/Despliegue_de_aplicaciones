a589a599ebd4d7f07be88c213337a432ff8caee8
Tehedor <101796030+Tehedor@users.noreply.github.com>
2024-01-05 03:18:57 +0100

    Update control_docker.py

--- 3_docker_seg/images/reviews/control_docker.py
+++ 3_docker_seg/images/reviews/control_docker.py
@@ -15,20 +15,11 @@ if not len(sys.argv) < 2:
 
 if segundo_argumento == "create":
     chdir('images/reviews')
-
-call(["cp","-r","../../practica_creativa2/bookinfo/src/reviews","."])
-
-# current_directory = os.getcwd()
-
-# run(["sudo", "docker", "run", "--rm", "-u", "root", "-v",current_directory + ":/home/gradle/project", "-w", "/home/gradle/project", "gradle:4.8.1", "gradle", "clean", "build"])
-# call(["sudo", "docker", "run", "--rm", "-u", "root", "-v", f"{current_directory}:/home/gradle/project", "-w", "/home/gradle/project", "gradle:4.8.1", "gradle", "clean", "build"])
-# call(["sudo", "docker", "run", "--rm", "-u", "root", "-v", f"{current_directory}:/home/gradle/project", "-w", "/home/gradle/project", "gradle:4.8.1", "gradle", "clean"])
-# print("hola")
-# call(["sudo", "docker", "run", "--rm", "-u", "root", "-v", f"{current_directory}:/home/gradle/project", "-w", "/home/gradle/project", "gradle:4.8.1", "gradle","build"])
-# print("hola2")
+    
+if not path.exists("./reviews"):
+    call(["cp","-r","../../../practica_creativa2/bookinfo/src/reviews","."])
 
 chdir('reviews/reviews-wlpcfg')
-
 
 for i,version in enumerate(versiones):
     enableRatings = "true"
